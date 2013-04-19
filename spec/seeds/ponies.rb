fcb8fcf2a8e690369af5551e04fd0ef2d04b12b7
Mike Poltyn <mike@poltyn.com>
2013-04-19 16:23:15 +0100

    add shiva rake task specs

---
+++ spec/seeds/ponies.rb
@@ -0,0 +1,4 @@
+Pony.create({name: 'Rainbow Dash'})
+Pony.create({name: 'Twilight Sparkle'})
+Pony.create({name: 'Princess Celestia'})
+Pony.create({name: 'DJ-PON3'})
--- spec/tasks/shiva_rake_spec.rb
+++ spec/tasks/shiva_rake_spec.rb
@@ -1,9 +1,28 @@
 require 'rake'
+require 'active_record'
+require 'models/pony'
+module ShivaSpec
+  class ShivaRakeDatabase < Shiva::Database
+    def migration_path
+      "spec/migrations/#{name}/"
+    end
+
+    def schema_path
+      "spec/tmp/#{name}_schema.rb"
+    end
+
+    def seeds_path
+      "spec/seeds/#{name}.rb"
+    end
+  end
+end
+
 
 describe 'shiva namespace rake task' do
   before :all do
     Rake.application.rake_require 'tasks/shiva'
     Rake::Task.define_task(:environment)
+    Shiva.configure do; end
   end
 
   context 'with empty configuration' do
@@ -70,6 +89,82 @@ describe 'shiva namespace rake task' do
     end
   end
 
-  pending "oh my, no tests for Rake task on proper instances :("
+  context 'with configuration' do
+    use_sqlite_database 'ponies'
+    before :each do
+      @database = ShivaSpec::ShivaRakeDatabase.new('Pony', 'ponies')
+      Shiva.configure do
+        @databases = [ShivaSpec::ShivaRakeDatabase.new('Pony', 'ponies')]
+      end
+    end
+
+    describe 'shiva:migrate' do
+      let :run_rake_task do
+        Rake::Task['shiva:migrate'].reenable
+        Rake.application.invoke_task 'shiva:migrate'
+      end
+
+      it 'runs!' do
+        run_rake_task
+      end
+    end
+
+    describe 'shiva:rollback' do
+      let :run_rake_task do
+        Rake::Task['shiva:rollback'].reenable
+        Rake.application.invoke_task 'shiva:rollback'
+      end
+
+      it 'runs!' do
+        run_rake_task
+      end
+    end
+
+    describe 'shiva:dump' do
+      let :run_rake_task do
+        Rake::Task['shiva:dump'].reenable
+        Rake.application.invoke_task 'shiva:dump'
+      end
+
+      it 'runs!' do
+        run_rake_task
+      end
+    end
+
+    describe 'shiva:abort_if_pending_migrations' do
+      let :run_rake_task do
+        Rake::Task['shiva:abort_if_pending_migrations'].reenable
+        Rake.application.invoke_task 'shiva:abort_if_pending_migrations'
+      end
+
+      it 'aborts on pending migrations!' do
+        lambda { run_rake_task }.should raise_error SystemExit
+      end
+
+      it 'runs for no pending migrations!' do
+        Shiva::Migrator.migrate @database
+        lambda { run_rake_task }.should_not raise_error SystemExit
+      end
+    end
+
+    # depends on #abort_if_pending_migrations
+    describe 'shiva:seeds' do
+      let :run_rake_task do
+        Rake::Task['shiva:seeds'].reenable
+        Rake::Task['shiva:abort_if_pending_migrations'].reenable
+        Rake.application.invoke_task 'shiva:seeds'
+      end
+
+      it 'aborts on pending migrations!' do
+        lambda { run_rake_task }.should raise_error SystemExit
+      end
+
+      it 'runs for no pending migrations!' do
+        Shiva::Migrator.migrate @database
+        lambda { run_rake_task }.should_not raise_error SystemExit
+      end
+
+    end
+  end
 
 end
