37edc1df2f70a75cb356c6fe26311a5f0e5da9d0
Mike Poltyn <mike@poltyn.com>
2013-04-19 11:32:50 +0100

    remove pry from monkeypatch

--- spec/support/ar_jdbc_sqlite3_monkey_patch.rb
+++ spec/support/ar_jdbc_sqlite3_monkey_patch.rb
@@ -2,10 +2,8 @@
 if defined?(JRUBY_VERSION)
   require 'arjdbc'
   require 'arjdbc/sqlite3'
-  require 'pry'
   ArJdbc::SQLite3.module_eval do
     def indexes(table_name, name = nil) # :nodoc:
-      binding.pry
       exec_query("PRAGMA index_list(#{quote_table_name(table_name)})", 'SCHEMA').map do |row|
         name = row['name']
         unique = row['unique'] != 0
